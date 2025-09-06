f344ea70886f48c33e7951f325df5dfb798dab5a
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-09-06 19:41:13 +0530

    LEETCODE

---
+++ RECURSION n BACKTRACKING/509.FibonacciNumber.cpp
@@ -0,0 +1,14 @@
+//   PROBLEM : FIBONACCI NUMBER
+//   LINK : https://leetcode.com/problems/fibonacci-number
+//   APPROACH : RECURSION 
+//   DIFFICULTY : EASY
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0) return 0 ;
+    if(n==1) return 1;
+    return fib(n-1)+fib(n-2);
+    }
+};
