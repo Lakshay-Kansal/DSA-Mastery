20261e47cd84b183ce0928bd6d3b67fadcf14900
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-09-18 15:48:13 +0530

    SORTING-TECHNIQUES

---
+++ SORTING/SelectionSort.cpp
@@ -0,0 +1,29 @@
+//   PROBLEM : SELECTION-SORT IMPLEMENTATION
+
+#include <bits/stdc++.h>
+using namespace std;
+// SELECTION-SORT FUNCTION
+void selectionsort(int arr[],int n){
+    for(int i =0; i<n-1;i++){
+        int smallest = i;
+        for(int j = i+1 ; j<n; j++){
+         if(arr[j]<arr[smallest]){
+            smallest = j;
+        }
+    }
+    swap(arr[i],arr[smallest]);
+    }
+}
+// PRINTING OF ARRAY
+void printarr(int arr[],int n){ 
+for(int i =0; i<n ; i++){
+    cout << arr[i] << " ";
+}
+}
+int main(){
+int n = 5;
+int arr[]={9,5,2,6,1};
+selectionsort(arr,n);   //function-call
+printarr(arr,n);     // print sorted array
+   return 0;
+}
