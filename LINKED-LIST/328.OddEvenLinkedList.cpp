14e3646a7ec49fae467281d3cc530309780ed40b
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-09-23 17:09:55 +0530

    LEETCODE

---
+++ LINKED-LIST/328.OddEvenLinkedList.cpp
@@ -0,0 +1,33 @@
+//   PROBLEM : SEGRREGATE ODD-EVEN NODE IN LINKED-LIST
+//   LINK : https://leetcode.com/problems/odd-even-linked-list
+//   DIFFICULTY : MEDIUM
+//   APPROACH : TWO-POINTER APPROACH
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenhead = head->next;
+        while(even != NULL && even->next != NULL){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+             
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenhead;
+        return head;
+    }
+};
