d1ba7c19b274bb59df2e6cddc2fd6fcd4afdd87c
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-09-24 13:41:21 +0530

    LEETCODE

---
+++ LINKED-LIST/876.MiddleOfTheLinkedList.cpp
@@ -0,0 +1,30 @@
+//   PROBLEM : MIDDLE OF THE LINKED-LIST
+//   LINK : https://leetcode.com/problems/middle-of-the-linked-list
+//   DIFFICULTY : EASY
+//   APPROACH : SLOW-FAST POINTER APPROACH : 
+//              SLOW-POINTER MOVES 1-STEP FAST-POINTER MOVES 2-STEP
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    if(head->next==NULL)return head;
+    while(fast != NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+    }  
+    return slow;
+    }
+};
