2db0e91e4d00156f9adda2eb121dcb34b6281a52
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-11-30 16:55:59 +0530

    LEETCODE

---
+++ TREES/BINARY SEARCH TREE/235.LowestCommonAncestor.cpp
@@ -0,0 +1,28 @@
+//   PROBLEM : LOWEST COMMON ANCESTOR OF A BINARY SEARCH TREE
+//   LINK : https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+//   DIFFICULTY : MEDIUM
+//   APPROACH : POINT WHERE PATH DIVERGES 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)return NULL;
+        if(p->val > root->val && q->val > root->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }else if(p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }else{
+            return root;
+        }
+    }
+};
