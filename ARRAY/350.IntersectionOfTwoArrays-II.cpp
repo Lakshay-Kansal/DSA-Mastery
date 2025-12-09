a742c57cf31bbc3823aaa8a87d5150555569b61a
LAKSHAY KANSAL <lakshaykansal1000@gmail.com>
2025-12-09 17:35:55 +0530

    LEETCODE

---
+++ ARRAY/350.IntersectionOfTwoArrays-II.cpp
@@ -0,0 +1,27 @@
+//   PROBLEM : INTERSECTION OF TWO ARRAYS - II
+//   LINK : https://leetcode.com/problems/intersection-of-two-arrays-ii
+//   DIFFICULTY : EASY
+//   APPROACH :  SORT THEN TWO-POINTER
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    sort(nums1.begin(),nums1.end());
+       sort(nums2.begin(),nums2.end());
+       int ptr1 = 0;
+       int ptr2 = 0;
+       vector<int>ans;
+       while(ptr1<nums1.size() && ptr2<nums2.size()){
+        if(nums1[ptr1] == nums2[ptr2]){
+            ans.push_back(nums1[ptr1]);
+            ptr1++;
+            ptr2++;
+        }else if(nums1[ptr1]>nums2[ptr2]){
+            ptr2++;
+        }else{
+            ptr1++;
+        }
+       } 
+       return ans;    
+    }
+};
